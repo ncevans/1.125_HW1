89db3c4000a591a9bd43e11ca4a5ba37b95f0ee0
Craig <ncevans@mit.edu>
2021-09-14 16:15:44 -0400

    First commit adding HW1

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var fetch = require('node-fetch');
+app.get('/', (req,res) => res.send('hello world!'));
+
+app.get('/buses', async (req,res) => {
+var url= 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+var response = await fetch(url);    
+var json = await response.json();
+res.send(json.data);
+})
+
+app.listen('3000', () => console.log('Running on 3000'));
